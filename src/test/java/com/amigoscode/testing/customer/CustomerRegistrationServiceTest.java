9c587fbb09289ce6e074d4993037933b1a815afd
kleberaluizio <kleberaluizio@gmail.com>
2023-09-18 15:39:12 -0300

    Testing service class

---
+++ src/test/java/com/amigoscode/testing/customer/CustomerRegistrationServiceTest.java
@@ -0,0 +1,53 @@
+package com.amigoscode.testing.customer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+class CustomerRegistrationServiceTest {
+
+    @Mock
+    private CustomerRepository customerRepository;
+
+    @Captor
+    private ArgumentCaptor<Customer> customerArgumentCaptor;
+
+    private CustomerRegistrationService underTest;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        underTest = new CustomerRegistrationService(customerRepository);
+    }
+
+    @Test
+    void itShouldSaveNewCustomer() {
+        // Given a phone number and a customer
+        String phoneNumber = "000099";
+        Customer customer = new Customer(UUID.randomUUID(),"Maryam",phoneNumber);
+
+        // ... a request
+        CustomerRegistrationRequest request = new CustomerRegistrationRequest(customer);
+
+        given(customerRepository.selectCustomerByPhoneNumber(phoneNumber))
+                .willReturn(Optional.empty());
+        // When
+        underTest.registerNewCustomer(request);
+
+        // Then
+        then(customerRepository).should().save(customerArgumentCaptor.capture());
+        Customer customerArgumentCaptorValue = customerArgumentCaptor.getValue();
+        assertThat(customerArgumentCaptorValue).isEqualToComparingFieldByField(customer);
+    }
+}
